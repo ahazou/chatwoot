d78bf15063c665e61fcb9919cf150fdcada092d5
Douglas <douglas@ahazou.com>
2024-08-06 17:05:26 -0300

    wip: building

--- db/migrate/20231201014644_remove_notifications_with_message_primary_actor.rb
+++ db/migrate/20231201014644_remove_notifications_with_message_primary_actor.rb
@@ -1,5 +1,5 @@
 class RemoveNotificationsWithMessagePrimaryActor < ActiveRecord::Migration[7.0]
   def change
-    Migration::RemoveMessageNotifications.perform_later
+    # Migration::RemoveMessageNotifications.perform_later
   end
 end
